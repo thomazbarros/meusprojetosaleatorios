2043b3a9a35ada7b064a4c92b4229f63aef02279
thomazavila <thomazavila@gmail.com>
2014-01-09 19:38:07 +0000

    BLAH

--- Peach/src/net/java/dev/netbeansspot/Application.java
+++ Peach/src/net/java/dev/netbeansspot/Application.java
@@ -7,12 +7,13 @@ import com.sun.spot.resources.transducers.LEDColor;
 import com.sun.spot.resources.transducers.ITriColorLEDArray;
 import com.sun.spot.resources.transducers.SwitchEvent;
 import com.sun.spot.io.j2me.radiogram.*;
-import com.sun.spot.peripheral.radio.IRadioPolicyManager;
+//import com.sun.spot.peripheral.radio.IRadioPolicyManager;
 import com.sun.spot.peripheral.radio.RadioFactory;
 import com.sun.spot.util.IEEEAddress;
 
 import java.io.*;
 import java.util.Hashtable;
+import java.util.Vector;
 import javax.microedition.io.*;
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.midlet.MIDletStateChangeException;
@@ -28,16 +29,20 @@ public class Application extends MIDlet implements ISwitchListener {
 
     private static final int CHANGE_COLOR = 1;
     private static final int CHANGE_COUNT = 2;
+    private static final int PEACH = 3;
+    private static final int DELAY = 100;
     private ITriColorLEDArray leds = (ITriColorLEDArray) Resources.lookup(ITriColorLEDArray.class);
     private ISwitch sw1 = (ISwitch) Resources.lookup(ISwitch.class, "SW1");
     private ISwitch sw2 = (ISwitch) Resources.lookup(ISwitch.class, "SW2");
     private int count = -1;
     private int color = 0;
     private LEDColor[] colors = {LEDColor.RED, LEDColor.GREEN, LEDColor.BLUE};
-    private RadiogramConnection tx = null, rx = null;
-    private Radiogram xdg, rdg;
+    private RadiogramConnection tx = null, tx2 = null, rx = null;
+    private Radiogram xdg, xdg2, rdg;
     private Hashtable um = null, dois = null, tres = null, quatro = null;
-    private String myaddress, sink, destination;
+    private String myaddress, mysink, mydestination, mynexthop, mypayload;
+    private String rcvaddress, rcvsink, rcvdestination, rcvpayload;
+    private Vector payload = null;
     //private IRadioPolicyManager rpm = (IRadioPolicyManager)Resources.lookup(IRadioPolicyManager.class);
     //private IEEEAddress myAddr = new IEEEAddress(rpm.getIEEEAddress());
 
@@ -59,9 +64,9 @@ public class Application extends MIDlet implements ISwitchListener {
 
     protected void startApp() throws MIDletStateChangeException {
 
-      myaddress = IEEEAddress.toDottedHex(RadioFactory.getRadioPolicyManager().getIEEEAddress());
-      System.out.println("*******AQUI********:"+myaddress);
-
+        myaddress = IEEEAddress.toDottedHex(RadioFactory.getRadioPolicyManager().getIEEEAddress());
+        System.out.println("*******AQUI********:" + myaddress);
+        payload = new Vector();
         um = new Hashtable();
         um.put("7f00.0101.0000.1002", "7f00.0101.0000.1002");
         um.put("7f00.0101.0000.1003", "7f00.0101.0000.1003");
@@ -91,6 +96,16 @@ public class Application extends MIDlet implements ISwitchListener {
         Thread t1 = new Thread(receive);
         t1.start();
 
+
+        if (myaddress.equals("7f00.0101.0000.1001")) {
+           mydestination="7f00.0101.0000.1003";
+           mynexthop="7f00.0101.0000.1003";
+           mysink="7f00.0101.0000.1004";
+           mypayload="DADO DO NODE UM!";
+            Send send = new Send();
+            Thread t2 = new Thread(send);
+            t2.start();
+        }
     }
 
     protected void pauseApp() {
@@ -118,7 +133,7 @@ public class Application extends MIDlet implements ISwitchListener {
             xdg.writeInt(cmd);
             xdg.writeInt(count);
             xdg.writeInt(color);
-          //  xdg.writeUTF(myaddress);
+            //xdg.writeUTF();
             tx.send(xdg);
         } catch (IOException ex) {
             System.out.println("Error sending packet: " + ex);
@@ -132,6 +147,17 @@ public class Application extends MIDlet implements ISwitchListener {
     public class Send implements Runnable {
 
         public void run() {
+            try {
+                xdg2.reset();
+                xdg2.writeInt(PEACH);
+                xdg2.writeInt(999);
+                xdg2.writeInt(888);
+                //xdg.writeUTF();
+                tx2.send(xdg2);
+            } catch (IOException ex) {
+                System.out.println("Error sending packet: " + ex);
+                ex.printStackTrace();
+            }
         }
     }
 
@@ -140,8 +166,12 @@ public class Application extends MIDlet implements ISwitchListener {
         public void run() {
 
             try {
+
+                System.out.println("ENVIO...");
                 tx = (RadiogramConnection) Connector.open("radiogram://broadcast:123");
                 xdg = (Radiogram) tx.newDatagram(20);
+                tx2 = (RadiogramConnection) Connector.open("radiogram://broadcast:123");
+                xdg2 = (Radiogram) tx2.newDatagram(20);
                 rx = (RadiogramConnection) Connector.open("radiogram://:123");
                 rdg = (Radiogram) rx.newDatagram(20);
                 while (true) {
@@ -150,14 +180,30 @@ public class Application extends MIDlet implements ISwitchListener {
                         int cmd = rdg.readInt();
                         int newCount = rdg.readInt();
                         int newColor = rdg.readInt();
-
+                        rcvdestination = rdg.getAddress();
+                        //rcvsink = rdg.readUTF();
+                        //rcvdestination = rdg.readUTF();
+                        //rcvpayload = rdg.readUTF();
                         //destination = rdg.readUTF();
                         if (cmd == CHANGE_COLOR) {
                             System.out.println("Received packet from " + rdg.getAddress());
-                           // System.out.println("MYADDRESS: "+rdg.readUTF());
+                            // System.out.println("MYADDRESS: "+rdg.readUTF());
                             showColor(newColor);
+                        } else if (cmd == PEACH) {
+                            //             if(rcvsink.equals(myaddress)){
+                            System.out.println("CONSUMINDO PACOTE!");
+                            System.out.println("Sou: " + myaddress);
+                            System.out.println("Destino final: " + rcvsink);
+                            System.out.println("Origem: ");
+                            System.out.println("newCount: " + newCount);
+                            System.out.println("newColor: " + newColor);
+                            System.out.println("Conteúdo: " + rcvpayload);
+                            System.out.println("##### FIM #####");
+                            //           }
+                            location_unware();
                         } else {
                             showCount(newCount, newColor);
+
                         }
                     } catch (IOException ex) {
                         System.out.println("Error receiving packet: " + ex);
@@ -170,5 +216,27 @@ public class Application extends MIDlet implements ISwitchListener {
             }
 
         }
+    }
+
+    public void location_unware() {
+        if (rcvdestination.equals(myaddress)) {
+            mynexthop = rcvsink;
+            formCluster(DELAY);
+            if (mynexthop.equals(rcvsink)) {
+                //selecionar para quem enviar a partir das tabelas
+            }
+            //enviar pacote
+        } else {
+            joinCluster();
+            if (!payload.isEmpty()) {
+                //enviar pacotes
+            }
+        }
+    }
+
+    public void formCluster(int delay) {
+    }
+
+    public void joinCluster() {
     }
 }
